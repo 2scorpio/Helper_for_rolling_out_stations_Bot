504b05c0e8c03df2062a2a82bcd878f335441941
UberPipa <nu.takoe.b@gmail.com>
2023-05-22 16:48:27 +0300

    Сделал отправку немного++

---
+++ tgBot/utility/data/output/UPDATE camera.sql
@@ -0,0 +1,2 @@
+UPDATE face_pay_ref_var.camera_ref set camera = 'mono_vystavochnyj_centr_v1_cam1' where place_id = 1305011;
+UPDATE face_pay_ref_var.camera_ref set camera = 'mono_vystavochnyj_centr_v2_cam1' where place_id = 1305021;
